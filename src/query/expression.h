03b14917e4a03f9c14faf52466dfe3af0620cb94
youngsamwei <w3wei@sina.com>
2018-01-17 19:50:15 +0800

    重新定义表达式

--- src/query/expression.h
+++ src/query/expression.h
@@ -5,68 +5,134 @@
 #ifndef DONGMENDB_EXPRESSION_H
 #define DONGMENDB_EXPRESSION_H
 
+#include "literal.h"
+
 /*定义表达式级别的结构*/
 
-typedef struct TableExpr_ TableExpr;
+enum OpType {
+    /*比较运算符*/
+            OP_EQ,
+    OP_LT,
+    OP_GT,
+    OP_LEQ,
+    OP_GEQ,
+    OP_IN,
+    OP_LIKE,
+    /*逻辑运算符*/
+            OP_AND,
+    OP_OR,
+    OP_NOT,
+    /*原子项*/
+            OP_TERM,
+    /*算术运算*/
+            OP_PLUS,
+    OP_MINUS,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEG,
+    /*字符串连接*/
+            OP_CONCAT
+};
+enum TermType {
+    TERM_LITERAL,
+    TERM_ID,
+    TERM_NULL,
+    TERM_COLREF,
+    TERM_FUNC
+};
+enum FuncType {
+    FUNC_MAX,
+    FUNC_MIN,
+    FUNC_COUNT,
+    FUNC_AVG,
+    FUNC_SUM
+};
+
+typedef struct TablesExpr_ TablesExpr;
 typedef struct WhereExpr_ WhereExpr;
+typedef struct Expression_ Expression;
+typedef struct TermExpr_ TermExpr;
+typedef struct BinaryExpr_ BinaryExpr;
+typedef struct UnaryExpr_ UnaryExpr;
+typedef struct FieldsExpr_ FieldsExpr;
+
+
+typedef struct FieldsExpr_ {
+    /*每一个field都是表达式*/
+    Expression *expr;
+    char *alias;
+    FieldsExpr *nextField;
+} FieldsExpr;
 
 /*当next为null时结尾*/
-typedef struct TableExpr_ {
+typedef struct TablesExpr_ {
     char *db;
     char *schema;
     char *name;
     /*用于连接当前Table与nextTable的条件*/
-    WhereExpr *joinExpr;
-    TableExpr *nextTable;
-} TableExpr;
+    Expression *joinExpr;
+    TablesExpr *nextTable;
+} TablesExpr;
 
-typedef struct ValueExpr_ ValueExpr;
-typedef struct FieldsExpr_ FieldsExpr;
-typedef struct FieldsExpr_ {
-    /*每一个field都是表达式*/
-    ValueExpr *valueExpr;
-    FieldsExpr *nextField;
-} FieldsExpr;
-
-/*逻辑运算的操作符*/
-enum OpType {
-    RA_OP_EQ,
-    RA_OP_LT,
-    RA_OP_GT,
-    RA_OP_LEQ,
-    RA_OP_GEQ,
-    RA_OP_IN,
-    RA_OP_LIKE,
-    RA_OP_AND,
-    RA_OP_OR,
-    RA_OP_NOT
-};
-
-/*值表达式：布尔，数值，字符串*/
-typedef struct ValueExpr_{
-
-}ValueExpr;
-
-typedef struct LogicalExpr_{
-    /*当opType为单目运算符时（如RA_OP_NOT）时，exprr有值*/
+typedef struct Expression_ {
+    /*当为OP_TERM时，nextExpression=null*/
     enum OpType opType;
-    ValueExpr *exprl, *exprr;
-} LogicalExpr;
+    union {
+        TermExpr *term;
+        BinaryExpr *binaryExpr;
+        UnaryExpr *unaryExpr;
+    } expr;
 
-typedef struct WhereExpr_ {
-    /*在where中最外层的cond的opType必须为逻辑运算符*/
-    LogicalExpr *cond;
-} WhereExpr;
+    Expression *nextExpression;
+} Expression;
 
+typedef struct Func {
+    enum FuncType t;
+    Expression *expr;
+} Func;
+
+/*原子表达式：标识符，常量，函数*/
+typedef struct TermExpr_ {
+    enum TermType t;
+    union {
+        char *id;
+        Literal *val;
+        Func f;
+    };
+} TermExpr;
+
+/*二元操作符表达式*/
+typedef struct BinaryExpr_ {
+    enum OpType opType;
+    Expression *leftExpr, *rightExpr;
+} BinaryExpr;
+
+/*一元操作符表达式*/
+typedef struct UnaryExpr_ {
+    enum OpType opType;
+    Expression *expr;
+} UnaryExpr;
+
+typedef struct GroupExpr_ GroupExpr;
 typedef struct GroupExpr_ {
-
+    Expression *expr;
+    GroupExpr *next;
 } GroupExpr;
 
+enum OrderType {
+    ORDER_BY_ASC, ORDER_BY_DESC
+};
+typedef struct OrderExpr_ OrderExpr;
 typedef struct OrderExpr_ {
-
+    enum OrderType orderType;
+    Expression *expr;
+    OrderExpr *next;
 } OrderExpr;
 
 typedef struct ColumnsExpr_ {
+    enum data_type dataType;
+    char *columnName;
+
 
 } ColumnsExpr;
 
