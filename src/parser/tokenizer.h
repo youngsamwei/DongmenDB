088c1c75f151731d681748fadb1569215a94446c
youngsamwei <w3wei@sina.com>
2018-01-19 09:39:37 +0800

    修改tokenizer适应sql语法

--- src/parser/tokenizer.h
+++ src/parser/tokenizer.h
@@ -18,53 +18,27 @@ typedef enum TokenType_{
     TOKEN_NOT_EQUAL,
     TOKEN_UNENDED_SRING,
     TOKEN_STRING,
-    TOKEN_MOD_EQUAL,
     TOKEN_MOD,
-    TOKEN_BITWISE_AND_EQUAL,
     TOKEN_AND,
-    TOKEN_BITWISE_AND,
     TOKEN_INCOMPLETE_CHAR,
     TOKEN_CHAR,
     TOKEN_INVALID_CHAR,
     TOKEN_OPEN_PAREN,
     TOKEN_CLOSE_PAREN,
-    TOKEN_MULTIPLY_EQUAL,
     TOKEN_MULTIPLY,
-    TOKEN_PLUS_EQUAL,       //+=
-    TOKEN_INCREMENT,        //++
     TOKEN_PLUS,
     TOKEN_COMMA,
-    TOKEN_MINUS_EQUAL,
-    TOKEN_DECREMENT,
-    TOKEN_STRUCT_POINT,
     TOKEN_MINUS,
     TOKEN_DOT,
-    TOKEN_DIVIDE_EQUAL,
     TOKEN_DIVIDE,
-    TOKEN_TERNARY_COLON,
     TOKEN_SEMICOLON,
     TOKEN_LE,               //less-than-or-equal-to operator"
-    TOKEN_LEFT_SHIFT_EQUAL,
-    TOKEN_LEFT_SHIFT,
     TOKEN_LT,              //less-than operator
     TOKEN_EQ,
     TOKEN_ASSIGNMENT,
     TOKEN_GE,
-    TOKEN_RIGHT_SHIFT_EQUAL,
-    TOKEN_RIGHT_SHIFT,
     TOKEN_GT,
-    TOKEN_TERNARY_QMARK,
-    TOKEN_OPEN_BRACKET,
-    TOKEN_CLOSE_BRACKET,
-    TOKEN_BIT_XOR,
-    TOKEN_OPEN_BRACE,
-    TOKEN_CLOSE_BRACE,
     TOKEN_OR,
-    TOKEN_BITWISE_EQUAL,
-    TOKEN_BITWISE_OR,
-    TOKEN_BITWISE_NOT_EQUAL,
-    TOKEN_BITWISE_NOT,
-    TOKEN_POUND,               //"include operator"
     TOKEN_EXP_FLOAT,
     TOKEN_FLOAT,
     TOKEN_OCTAL,               //"octal integer"
@@ -103,7 +77,6 @@ TokenT *_word(TokenizerT *tk);
 TokenT *_neq(TokenizerT *tk);
 TokenT *_double_quote(TokenizerT *tk);
 TokenT *_mod(TokenizerT *tk);
-TokenT *_bit_and(TokenizerT *tk);
 TokenT *_single_quote(TokenizerT *tk);
 TokenT *_open_paren(TokenizerT *tk);
 TokenT *_close_paren(TokenizerT *tk);
@@ -111,23 +84,13 @@ TokenT *_mult(TokenizerT *tk);
 TokenT *_plus(TokenizerT *tk);
 TokenT *_comma(TokenizerT *tk);
 TokenT *_minus(TokenizerT *tk);
-TokenT *_dot(TokenizerT *tk);
 TokenT *_line_comment(TokenizerT *tk);
 TokenT *_block_comment(TokenizerT *tk);
 TokenT *_div(TokenizerT *tk);
-TokenT *_ternary_colon(TokenizerT *tk);
 TokenT *_semicolon(TokenizerT *tk);
 TokenT *_lt(TokenizerT *tk);
 TokenT *_eq(TokenizerT *tk);
 TokenT *_gt(TokenizerT *tk);
-TokenT *_open_bracket(TokenizerT *tk);
-TokenT *_close_bracket(TokenizerT *tk);
-TokenT *_bit_xor(TokenizerT *tk);
-TokenT *_open_brace(TokenizerT *tk);
-TokenT *_bit_or(TokenizerT *tk);
-TokenT *_close_brace(TokenizerT *tk);
-TokenT *_bit_not(TokenizerT *tk);
-TokenT *_pound(TokenizerT *tk);
 TokenT *_expofloat(TokenizerT *tk, int isFirst, int lastWasSign);
 TokenT *_float(TokenizerT *tk, int isFirst);
 TokenT *_octal(TokenizerT *tk);
@@ -135,7 +98,6 @@ TokenT *_hex(TokenizerT *tk, int isFirst);
 TokenT *_decimal(TokenizerT *tk);
 TokenT *_zero(TokenizerT *tk);
 TokenT *TKGetNextToken(TokenizerT *tk);
-
 
 
 #endif //DONGMENDB_TOKENIZER_H
