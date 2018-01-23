88d509d90b0f13463f634bc0c69ab2b57eae1237
youngsamwei <w3wei@sina.com>
2018-01-23 10:43:39 +0800

    开始尝试解析select

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -9,6 +9,11 @@
 #include "parseExpression.h"
 #include "parseExpressionRD.h"
 
+/**
+ * @brief 创建语法解析器
+ * @param tokenizer 词法分析器
+ * @return 解析器
+ */
 ParserT *newParser(TokenizerT *tokenizer) {
     ParserT *parser = (ParserT *) malloc(sizeof(ParserT));
     parser->tokenizer = tokenizer;
@@ -17,7 +22,12 @@ ParserT *newParser(TokenizerT *tokenizer) {
     return parser;
 };
 
-/*如果当前的token不为NULL，则不获取下一个token*/
+/**
+ * @brief 获取下一个token，如果当前的token不为NULL，则不获取下一个token。
+ * 如果解析出错则返回NULL,停止解析
+ * @param parser 解析器
+ * @return Token
+ */
 TokenT *parseNextToken(ParserT *parser) {
     if (parser->parserStateType==PARSER_WRONG){
         return NULL;
@@ -28,7 +38,11 @@ TokenT *parseNextToken(ParserT *parser) {
     return parser->currToken;
 };
 
-/*在处理完当前token后，需要调用此方法。*/
+/**
+ * @brief 处理完一个token后，需要调用此方法把token销毁。
+ * @param parser  解析器
+ * @return  NULL
+ */
 TokenT *parseEatToken(ParserT *parser) {
     if (parser->parserStateType==PARSER_WRONG){
         return NULL;
@@ -37,7 +51,11 @@ TokenT *parseEatToken(ParserT *parser) {
     return NULL;
 };
 
-/*eat掉当前的token，并获取下一个token*/
+/**
+ * @brief eat掉当前的token，并获取下一个token
+ * @param parser
+ * @return
+ */
 TokenT *parseEatAndNextToken(ParserT *parser) {
     if (parser->parserStateType==PARSER_WRONG){
         return NULL;
@@ -46,22 +64,42 @@ TokenT *parseEatAndNextToken(ParserT *parser) {
     return parser->currToken;
 };
 
+/**
+ * @brief 产生解析错误
+ * @param parser 解析器
+ * @param message 错误消息
+ * @return NULL
+ */
 void *parseError(ParserT *parser, char *message) {
     strcpy(parser->parserMessage, message);
     parser->parserStateType = PARSER_WRONG;
     return NULL;
 };
 
+/**
+ * @brief 解析select语句
+ * @param parser 解析器
+ * @return select语句
+ */
 SelectStmt *parseSelect(ParserT *parser) {
-    FieldsExpr *fieldsExpr = parseFieldsExpr(parser);
     TokenT *token = parseNextToken(parser);
-    if (stricmp(token->text, "from") != 0) {
+    if (stricmp(token->text, "select") ==0){
+        token = parseEatAndNextToken(parser);
+    }else{
         strcpy(parser->parserMessage , "语法错误.");
         return NULL;
     }
+    FieldsExpr *fieldsExpr = parseFieldsExpr(parser);
+    token = parseNextToken(parser);
+    if (stricmp(token->text, "from") != 0) {
+        strcpy(parser->parserMessage , "语法错误.");
+        return NULL;
+    }else{
+        token = parseEatAndNextToken(parser);
+    }
     TablesExpr *tablesExpr = parseTablesExpr(parser);
-    token = parseNextToken(parser);;
-    if (token == NULL) {
+    token = parseNextToken(parser);
+    if (token == NULL || token->type ==TOKEN_SEMICOLON) {
         return createSelectStmt(fieldsExpr, tablesExpr, NULL, NULL, NULL);
     }
     if (stricmp(token->text, "where") != 0) {
@@ -98,33 +136,56 @@ SelectStmt *parseSelect(ParserT *parser) {
     token = parseNextToken(parser);;
     if (token == NULL) {
         return createSelectStmt(fieldsExpr, tablesExpr, whereExpr, groupExpr, orderExpr);
+    }else{
+        return NULL;
     }
 
 };
 
-CreateStmt *parseCreate(ParserT *parser) {};
+CreateStmt *parseCreate(ParserT *parser) {return NULL;};
 
-AlterStmt *parseAlter(ParserT *parser) {};
+AlterStmt *parseAlter(ParserT *parser) {return NULL;};
 
-DeleteStmt *parseDelete(ParserT *parser) {};
+DeleteStmt *parseDelete(ParserT *parser) {return NULL;};
 
-UpdateStmt *parseUpdate(ParserT *parser) {};
+UpdateStmt *parseUpdate(ParserT *parser) {return NULL;};
 
-FieldsExpr *parseFieldsExpr(ParserT *parser) {};
+FieldsExpr *parseFieldsExpr(ParserT *parser) {
+    Expression *expr0 = parseExpressionRD(parser);
+    FieldsExpr *fieldsExpr = (FieldsExpr *)malloc(sizeof(FieldsExpr));
+    fieldsExpr->alias=NULL;
+    fieldsExpr->nextField = NULL;
+    fieldsExpr->expr = expr0;
+    return fieldsExpr;
+};
 
-TablesExpr *parseTablesExpr(ParserT *parser) {};
+TablesExpr *parseTablesExpr(ParserT *parser) {
+    TokenT *token = parseNextToken(parser);
+    if (token->type==TOKEN_WORD){
+        parseEatAndNextToken(parser);
+        TablesExpr *tablesExpr = (TablesExpr *)malloc(sizeof(TablesExpr));
+        tablesExpr->db=NULL;
+        tablesExpr->joinExpr=NULL;
+        tablesExpr->name = token->text;
+        tablesExpr->nextTable = NULL;
+        tablesExpr->schema = NULL;
+        return tablesExpr;
+    }
 
-TermExpr *parseTermExpr(ParserT *parser) {};
+    return NULL;
+};
 
-BinaryExpr *parseBinaryExpr(ParserT *parser) {};
+TermExpr *parseTermExpr(ParserT *parser) {return NULL;};
 
-UnaryExpr *parseUnaryExpr(ParserT *parser) {};
+BinaryExpr *parseBinaryExpr(ParserT *parser) {return NULL;};
 
-GroupExpr *parseGroupExpr(ParserT *parser) {};
+UnaryExpr *parseUnaryExpr(ParserT *parser) {return NULL;};
 
-OrderExpr *parseOrderExpr(ParserT *parser) {};
+GroupExpr *parseGroupExpr(ParserT *parser) {return NULL;};
 
-ColumnsExpr *parseColumnsExpr(ParserT *parser) {};
+OrderExpr *parseOrderExpr(ParserT *parser) {return NULL;};
 
-SetExpr *parseSetExpr(ParserT *parser) {};
+ColumnsExpr *parseColumnsExpr(ParserT *parser) {return NULL;};
+
+SetExpr *parseSetExpr(ParserT *parser) {return NULL;};
 
