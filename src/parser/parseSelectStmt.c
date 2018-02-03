b4c18364c9a3c0da957f2b3db992d477cdc75b20
youngsamwei <w3wei@sina.com>
2018-02-03 10:16:20 +0800

    解析insert语句 insert into tablename(field1,field2,..., fieldn) values(constant_value1, constant_value2,...,constant_valuen);

--- src/parser/parseSelectStmt.c
+++ src/parser/parseSelectStmt.c
@@ -184,7 +184,7 @@ sql_stmt_create *parse_sql_stmt_create(ParserT *parser) {
             arraylist_add(fieldsName, field->fieldName);
         }
         token = parseNextToken(parser);
-        if (token->type==TOKEN_COMMA){
+        if (token->type == TOKEN_COMMA) {
             token = parseEatAndNextToken(parser);
         } else {
             break;
@@ -225,9 +225,9 @@ field_info *parse_sql_stmt_columnexpr(ParserT *parser) {
                 if (token->type == TOKEN_DECIMAL) {
                     length = atoi(token->text);
                     token = parseEatAndNextToken(parser);
-                    if(matchToken(parser, TOKEN_CLOSE_PAREN, ")")){
+                    if (matchToken(parser, TOKEN_CLOSE_PAREN, ")")) {
                         token = parseNextToken(parser);
-                    }else{
+                    } else {
                         strcpy(parser->parserMessage, "invalid sql: missing ).");
                         return NULL;
                     }
@@ -254,3 +254,92 @@ field_info *parse_sql_stmt_columnexpr(ParserT *parser) {
     field->fieldName = columnName;
     return field;
 };
+
+sql_stmt_insert *parse_sql_stmt_insert(ParserT *parser) {
+    char *tableName = NULL;
+    arraylist *fields = arraylist_create();
+    arraylist *values = arraylist_create();
+
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "insert")) {
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "into")) {
+        strcpy(parser->parserMessage, "invalid sql: should be into.");
+        return NULL;
+    }
+    TokenT *token = parseNextToken(parser);
+    if (token->type == TOKEN_WORD) {
+        tableName = new_id_name();
+        strcpy(tableName, token->text);
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing table name.");
+        return NULL;
+    }
+    token = parseEatAndNextToken(parser);
+    if (!matchToken(parser, TOKEN_OPEN_PAREN, "(")) {
+        strcpy(parser->parserMessage, "invalid sql: missing (.");
+        return NULL;
+    }
+    token = parseNextToken(parser);
+    if (token->type == TOKEN_WORD) {
+        while (token->type == TOKEN_WORD) {
+            char *fieldName = new_id_name();
+            strcpy(fieldName, token->text);
+            arraylist_add(fields, fieldName);
+            token = parseEatAndNextToken(parser);
+            if (token->type==TOKEN_COMMA){
+                token = parseEatAndNextToken(parser);
+            }else{
+                break;
+            }
+        }
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing field name.");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_CLOSE_PAREN, ")")) {
+        strcpy(parser->parserMessage, "invalid sql: missing ).");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "values")) {
+        strcpy(parser->parserMessage, "invalid sql: missing values.");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_OPEN_PAREN, "(")) {
+        strcpy(parser->parserMessage, "invalid sql: missing (.");
+        return NULL;
+    }
+    token = parseNextToken(parser);
+    if (token->type == TOKEN_STRING || token->type == TOKEN_DECIMAL) {
+        while (token->type == TOKEN_STRING || token->type == TOKEN_DECIMAL) {
+            if (token->type == TOKEN_STRING){
+                char *value = strdup(token->text);
+                arraylist_add(values, value);
+            } else{
+                integer *i = (integer *)calloc(sizeof(integer *), 1);
+                i->val = atoi(token->text);
+                arraylist_add(values, i);
+            }
+
+            token = parseEatAndNextToken(parser);
+            if (token->type==TOKEN_COMMA){
+                token = parseEatAndNextToken(parser);
+            }else{
+                break;
+            }
+        }
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing a value.");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_CLOSE_PAREN, ")")) {
+        strcpy(parser->parserMessage, "invalid sql: missing ).");
+        return NULL;
+    }
+    sql_stmt_insert *sqlStmtInsert = (sql_stmt_insert *)calloc(sizeof(sql_stmt_insert),1);
+    sqlStmtInsert->tableName = tableName;
+    sqlStmtInsert->fields = fields;
+    sqlStmtInsert->values = values;
+    return sqlStmtInsert;
+};
+
