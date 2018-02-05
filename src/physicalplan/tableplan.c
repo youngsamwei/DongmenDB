9da2f7977b9576788289b51e9c274907fc073df0
youngsamwei <w3wei@sina.com>
2018-02-05 19:05:02 +0800

    准备实现关系代数表达式

--- src/physicalplan/tableplan.c
+++ src/physicalplan/tableplan.c
@@ -3,11 +3,13 @@
 //
 
 #include "tableplan.h"
-table_plan *table_plan_create(dongmengdb *db, char *tableName, transaction *tx){
-    table_plan *tablePlan = (table_plan *)calloc(sizeof(table_scan),1);
-    tablePlan->tableInfo =  table_manager_get_tableinfo(db->metadataManager->tableManager, tableName, tx);
+
+table_plan *table_plan_create(dongmengdb *db, char *tableName, transaction *tx) {
+    table_plan *tablePlan = (table_plan *) calloc(sizeof(table_scan), 1);
+    tablePlan->tableInfo = table_manager_get_tableinfo(db->metadataManager->tableManager, tableName, tx);
     tablePlan->tableScan = table_scan_create(tablePlan->tableInfo, tx);
 };
-table_scan *table_plan_open(table_plan *tablePlan){
+
+table_scan *table_plan_open(table_plan *tablePlan) {
     return tablePlan->tableScan;
 };
