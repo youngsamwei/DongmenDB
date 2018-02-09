52edf8f4cefd33309513ae4205226a48849c9dba
youngsamwei <w3wei@sina.com>
2018-02-09 10:01:51 +0800

    增加测试数据student, sc, course

--- src_test/sqlscripts/create.sql
+++ src_test/sqlscripts/create.sql
@@ -1,4 +1,6 @@
 create table student(sno char(10), sname char(20), sage integer);
+create table course(cno char(10), cname char(20), credit integer);
+create table sc(sno char(10), cno char(10), grade integer);
 
 .open demodb
 
@@ -12,6 +14,26 @@ insert into student(sno, sname, sage) values("2012010106","mary simith",25);
 insert into student(sno, sname, sage) values("2012010107","jene simith",26);
 insert into student(sno, sname, sage) values("2012010108","di simith",27);
 insert into student(sno, sname, sage) values("2012010108","sam simith",27);
+
+insert into course(cno, cname, credit) values("c001", "data structure", 3);
+insert into course(cno, cname, credit) values("c002", "c", 4);
+insert into course(cno, cname, credit) values("c003", "database", 3);
+insert into course(cno, cname, credit) values("c004", "math", 6);
+
+insert into sc(sno, cno, grade) values("2012010101","c001",80);
+insert into sc(sno, cno, grade) values("2012010102","c001",45);
+insert into sc(sno, cno, grade) values("2012010103","c001",75);
+insert into sc(sno, cno, grade) values("2012010104","c001",77);
+insert into sc(sno, cno, grade) values("2012010105","c001",67);
+insert into sc(sno, cno, grade) values("2012010106","c001",87);
+insert into sc(sno, cno, grade) values("2012010107","c001",90);
+insert into sc(sno, cno, grade) values("2012010101","c002",96);
+insert into sc(sno, cno, grade) values("2012010102","c002",74);
+insert into sc(sno, cno, grade) values("2012010103","c002",86);
+insert into sc(sno, cno, grade) values("2012010104","c003",95);
+insert into sc(sno, cno, grade) values("2012010105","c003",89);
+insert into sc(sno, cno, grade) values("2012010106","c003",48);
+insert into sc(sno, cno, grade) values("2012010107","c003",80);
 
 
 .exit
