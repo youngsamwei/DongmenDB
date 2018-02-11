a01029a3368603bfbc97ecbeb1bac873de83069c
youngsamwei <w3wei@sina.com>
2018-02-11 11:06:43 +0800

    开始准备测试

---
+++ src_test/select/test_select.c
@@ -0,0 +1,15 @@
+//
+// Created by Sam on 2018/2/11.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    printf("is %d\n", a+b);
+    printf("exit 0!\n");
+    return 0;
+}
