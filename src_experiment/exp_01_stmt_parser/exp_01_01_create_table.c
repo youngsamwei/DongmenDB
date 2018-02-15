6d457e4b82a1ad88e2f98924db5228bc9eb01868
youngsamwei <w3wei@sina.com>
2018-02-15 18:41:11 +0800

    增加语句解析实验要求

--- src_experiment/exp_01_stmt_parser/exp_01_01_create_table.c
+++ src_experiment/exp_01_stmt_parser/exp_01_01_create_table.c
@@ -2,3 +2,7 @@
 // Created by Sam on 2018/2/13.
 //
 
+/**
+ * 在现有create table基础上，修改代码以支持pk，fk，check，unique，not null约束。
+ *
+ * /
--- src_experiment/exp_01_stmt_parser/exp_01_02_insert.c
+++ src_experiment/exp_01_stmt_parser/exp_01_02_insert.c
@@ -2,3 +2,7 @@
 // Created by Sam on 2018/2/13.
 //
 
+/**
+ * 在现有实现的基础上，修改parser以支持insert values多条的特性。
+ * insert into table(f1,f2,...,fn) values (v1,v2,...,vn),(v1,v2,...,vn),(v1,v2,...,vn)(v1,v2,...,vn);
+ */
