f49ecc72e538a78fc8d6fc0d3ac519d9d8c6181d
youngsamwei <w3wei@sina.com>
2018-02-15 18:34:42 +0800

    增加完整性检查的实验

---
+++ src_experiment/exp_03_integrity/exp_03_01_integrity_check.c
@@ -0,0 +1,18 @@
+//
+// Created by Sam on 2018/2/13.
+//
+
+#include <dongmendb/integritymanager.h>
+
+int integrity_manager_entity_constraint_check(integrity_manager *integrityManager, table_info *tableInfo, record_value *recordValue, sql_update_type sqlUpdateType){
+
+};
+int integrity_manager_reference_constraint_check(integrity_manager *integrityManager, table_info *tableInfo, record_value *recordValue, sql_update_type sqlUpdateType){
+
+};
+int integrity_manager_check_constraint_check(integrity_manager *integrityManager, table_info *tableInfo, record_value *recordValue){
+
+};
+int integrity_manager_null_constraint_check(integrity_manager *integrityManager, table_info *tableInfo, record_value *recordValue){
+
+};
