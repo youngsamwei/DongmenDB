faf93229ed8a22913671cae0a99a32c210854756
youngsamwei <w3wei@sina.com>
2018-02-15 19:24:13 +0800

    将create table，select，insert语句的解析放到实验内容里，在此基础删修改

--- src_experiment/exp_01_stmt_parser/exp_01_01_create_table.c
+++ src_experiment/exp_01_stmt_parser/exp_01_01_create_table.c
@@ -2,7 +2,117 @@
 // Created by Sam on 2018/2/13.
 //
 
+#include "parser/parseSelectStmt.h"
+#include <parser/statement.h>
+#include <parser/parser.h>
+#include <utils/utils.h>
+
 /**
  * 在现有create table基础上，修改代码以支持pk，fk，check，unique，not null约束。
- *
- * /
+ */
+
+sql_stmt_create *parse_sql_stmt_create(ParserT *parser) {
+    char *tableName = NULL;
+    hmap_t columns = hashmap_create();
+    arraylist *fieldsName = arraylist_create();
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "create")) {
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "table")) {
+        strcpy(parser->parserMessage, "invalid sql: should be table.");
+        return NULL;
+    }
+    TokenT *token = parseNextToken(parser);
+    if (token->type == TOKEN_WORD) {
+        tableName = new_id_name();
+        strcpy(tableName, token->text);
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing table name.");
+        return NULL;
+    }
+    token = parseEatAndNextToken(parser);
+    if (!matchToken(parser, TOKEN_OPEN_PAREN, "(")) {
+        strcpy(parser->parserMessage, "invalid sql: missing (.");
+        return NULL;
+    }
+    token = parseNextToken(parser);
+
+    while (token->type != TOKEN_CLOSE_PAREN) {
+        field_info *field = parse_sql_stmt_columnexpr(parser);
+        if (field == NULL) {
+            break;
+        } else {
+            hashmap_put(columns, field->fieldName, field);
+            arraylist_add(fieldsName, field->fieldName);
+        }
+        token = parseNextToken(parser);
+        if (token->type == TOKEN_COMMA) {
+            token = parseEatAndNextToken(parser);
+        } else {
+            break;
+        }
+    }
+    token = parseNextToken(parser);
+    if (!matchToken(parser, TOKEN_CLOSE_PAREN, ")")) {
+        strcpy(parser->parserMessage, "invalid sql: missing ).");
+        return NULL;
+    }
+
+    return sql_stmt_create_create(tableName, fieldsName, columns, NULL);
+};
+
+field_info *parse_sql_stmt_columnexpr(ParserT *parser) {
+    TokenT *token = parseNextToken(parser);
+    char *columnName = NULL;
+    enum data_type type;
+    int length;
+    if (token->type == TOKEN_WORD) {
+        columnName = new_id_name();
+        strcpy(columnName, token->text);
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing field name.");
+        return NULL;
+    }
+    token = parseEatAndNextToken(parser);
+    if (token->type == TOKEN_RESERVED_WORD) {
+        if (stricmp(token->text, "int") == 0 || stricmp(token->text, "integer") == 0) {
+            type = DATA_TYPE_INT;
+            length = INT_SIZE;
+            token = parseEatAndNextToken(parser);
+        } else if (stricmp(token->text, "char") == 0) {
+            type = DATA_TYPE_CHAR;
+            token = parseEatAndNextToken(parser);
+            if (matchToken(parser, TOKEN_OPEN_PAREN, "(")) {
+                token = parseNextToken(parser);
+                if (token->type == TOKEN_DECIMAL) {
+                    length = atoi(token->text);
+                    token = parseEatAndNextToken(parser);
+                    if (matchToken(parser, TOKEN_CLOSE_PAREN, ")")) {
+                        token = parseNextToken(parser);
+                    } else {
+                        strcpy(parser->parserMessage, "invalid sql: missing ).");
+                        return NULL;
+                    }
+                } else {
+                    strcpy(parser->parserMessage, "invalid sql: missing char length.");
+                    return NULL;
+                }
+            } else {
+                strcpy(parser->parserMessage, "invalid sql: missing char length.");
+                return NULL;
+            }
+        } else {
+            strcpy(parser->parserMessage, "invalid sql: wrong data type : ");
+            strcat(parser->parserMessage, token->text);
+            return NULL;
+        }
+    } else {
+        strcpy(parser->parserMessage, "invalid sql : missing field name.");
+        return NULL;
+    }
+    field_info *field = (field_info *) calloc(sizeof(field_info), 1);
+    field->type = type;
+    field->length = length;
+    field->fieldName = columnName;
+    return field;
+};
--- src_experiment/exp_01_stmt_parser/exp_01_02_insert.c
+++ src_experiment/exp_01_stmt_parser/exp_01_02_insert.c
@@ -1,8 +1,107 @@
 //
 // Created by Sam on 2018/2/13.
 //
+#include "parser/parseSelectStmt.h"
+#include <parser/statement.h>
+#include <parser/parser.h>
+#include <utils/utils.h>
 
 /**
  * 在现有实现的基础上，修改parser以支持insert values多条的特性。
  * insert into table(f1,f2,...,fn) values (v1,v2,...,vn),(v1,v2,...,vn),(v1,v2,...,vn)(v1,v2,...,vn);
  */
+
+sql_stmt_insert *parse_sql_stmt_insert(ParserT *parser) {
+    char *tableName = NULL;
+    arraylist *fields = arraylist_create();
+    arraylist *values = arraylist_create();
+
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "insert")) {
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "into")) {
+        strcpy(parser->parserMessage, "invalid sql: should be into.");
+        return NULL;
+    }
+    TokenT *token = parseNextToken(parser);
+    if (token->type == TOKEN_WORD) {
+        tableName = new_id_name();
+        strcpy(tableName, token->text);
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing table name.");
+        return NULL;
+    }
+    token = parseEatAndNextToken(parser);
+    if (!matchToken(parser, TOKEN_OPEN_PAREN, "(")) {
+        strcpy(parser->parserMessage, "invalid sql: missing (.");
+        return NULL;
+    }
+    token = parseNextToken(parser);
+    if (token->type == TOKEN_WORD) {
+        while (token->type == TOKEN_WORD) {
+            char *fieldName = new_id_name();
+            strcpy(fieldName, token->text);
+            arraylist_add(fields, fieldName);
+            token = parseEatAndNextToken(parser);
+            if (token->type==TOKEN_COMMA){
+                token = parseEatAndNextToken(parser);
+            }else{
+                break;
+            }
+        }
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing field name.");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_CLOSE_PAREN, ")")) {
+        strcpy(parser->parserMessage, "invalid sql: missing ).");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_RESERVED_WORD, "values")) {
+        strcpy(parser->parserMessage, "invalid sql: missing values.");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_OPEN_PAREN, "(")) {
+        strcpy(parser->parserMessage, "invalid sql: missing (.");
+        return NULL;
+    }
+    token = parseNextToken(parser);
+    if (token->type == TOKEN_STRING || token->type == TOKEN_DECIMAL) {
+        while (token->type == TOKEN_STRING || token->type == TOKEN_DECIMAL) {
+            if (token->type == TOKEN_STRING){
+                /*去掉引号*/
+                int len = strlen(token->text) - 1;
+                char *v = token->text + 1;
+                char *value = (char *) calloc(len, 1);
+                strcpy(value, v);
+                value[len-1] = '\0';
+
+                arraylist_add(values, value);
+            } else{
+                integer *i = (integer *)calloc(sizeof(integer *), 1);
+                i->val = atoi(token->text);
+                arraylist_add(values, i);
+            }
+
+            token = parseEatAndNextToken(parser);
+            if (token->type==TOKEN_COMMA){
+                token = parseEatAndNextToken(parser);
+            }else{
+                break;
+            }
+        }
+    } else {
+        strcpy(parser->parserMessage, "invalid sql: missing a value.");
+        return NULL;
+    }
+    if (!matchToken(parser, TOKEN_CLOSE_PAREN, ")")) {
+        strcpy(parser->parserMessage, "invalid sql: missing ).");
+        return NULL;
+    }
+    sql_stmt_insert *sqlStmtInsert = (sql_stmt_insert *)calloc(sizeof(sql_stmt_insert),1);
+    sqlStmtInsert->tableName = tableName;
+    sqlStmtInsert->fields = fields;
+    sqlStmtInsert->values = values;
+    return sqlStmtInsert;
+};
+
