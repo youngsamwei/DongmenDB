500a01ed3aac091ff8d2d26e5dd643a5427988a4
youngsamwei <w3wei@sina.com>
2018-09-18 15:43:32 +0800

    细化需要支持的update和delete语句的语法

--- src_experiment/exp_01_stmt_parser/exp_01_05_delete.c
+++ src_experiment/exp_01_stmt_parser/exp_01_05_delete.c
@@ -7,6 +7,11 @@
 /**
  * 在现有实现基础上，实现delete from子句
  *
+ *  支持的delete语法：
+ *
+ *  DELETE FROM <table_nbame>
+ *  WHERE <logical_expr>
+ *
  * 解析获得 sql_stmt_delete 结构
  */
 
--- src_experiment/exp_07_physical_operate/exp_07_05_update.c
+++ src_experiment/exp_07_physical_operate/exp_07_05_update.c
@@ -4,7 +4,7 @@
 #include "physicalplan/physicalplan.h"
 
 /*执行 update 语句*/
-
+/*TODO: plan_execute_update， update语句执行*/
 int plan_execute_update(dongmendb *db, sql_stmt_update *sqlStmtUpdate , transaction *tx){
     /*删除语句以select的物理操作为基础实现。
      * 1. 使用 sql_stmt_update 的条件参数，调用 physical_scan_select_create 创建select的物理计划并初始化;
