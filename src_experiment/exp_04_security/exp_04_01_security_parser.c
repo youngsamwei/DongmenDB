4b31069723c986aa70f906e3c9338c3dcf00c0ab
youngsamwei <w3wei@sina.com>
2018-09-22 11:02:14 +0800

    完善完整性管理和安全性管理。

--- src_experiment/exp_04_security/exp_04_01_security_parser.c
+++ src_experiment/exp_04_security/exp_04_01_security_parser.c
@@ -6,6 +6,12 @@
 #include <dongmendb/securitymanager.h>
 #include <parser/statement.h>
 /**
+ * 安全性管理实验需要实现：
+ * 1. 安全性定义：语句解析
+ * 2. 安全性存储管理：系统数据表结构
+ * 3. 权限验证：当select，update，delete，insert时检查该用户是否有相应的权限
+ * 4. 处理：合法则放行，违法则拒绝。
+ *
  * 实现以下语句的解析：
  * 1）create user | role ,
  * 2）grant right on table  to user
