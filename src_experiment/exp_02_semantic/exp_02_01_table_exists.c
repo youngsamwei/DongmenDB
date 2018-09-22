58ff539a6d5b9e178f86ccab9e2f0e169093224e
youngsamwei <w3wei@sina.com>
2018-09-22 09:58:41 +0800

    检查表与字段是否存在

--- src_experiment/exp_02_semantic/exp_02_01_table_exists.c
+++ src_experiment/exp_02_semantic/exp_02_01_table_exists.c
@@ -2,3 +2,11 @@
 // Created by Sam on 2018/2/13.
 //
 
+/*
+ * 检查是否存在数据表
+ * 此函数在以下情况会被调用：
+ * 1. 执行select，update，insert，delete时调用，检查表是否存在
+ * 2 执行create table语句时检查表是否已经存在
+ * 3 执行create index时检查表是否存在
+ *
+ * */
--- src_experiment/exp_02_semantic/exp_02_02_field_exists.c
+++ src_experiment/exp_02_semantic/exp_02_02_field_exists.c
@@ -2,3 +2,9 @@
 // Created by Sam on 2018/2/13.
 //
 
+/*
+ * 检查字段是否存在
+ * 此函数在以下情况会被调用：
+ * 1. 执行select，update，insert，delete时调用，检查相关字段是否存在
+ * 2  执行create index时检查相关字段是否存在
+ * */
