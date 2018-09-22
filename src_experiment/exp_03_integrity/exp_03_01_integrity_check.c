f1eb6e546376f80fa24b9aa7010c629b3b53624c
youngsamwei <w3wei@sina.com>
2018-09-22 11:02:14 +0800

    完善完整性管理和安全性管理。

--- src_experiment/exp_03_integrity/exp_03_01_integrity_check.c
+++ src_experiment/exp_03_integrity/exp_03_01_integrity_check.c
@@ -4,6 +4,15 @@
 
 #include <dongmendb/integritymanager.h>
 
+/*
+ * 完整性管理实验需要实现：
+ * 1. 完整性定义：完整性约束的语句解析
+ * 2. 完整性约束存储：存储在什么结构的数据表中；
+ * 3. 完整性检查：当insert，update，delete时检查完整性约束
+ * 4. 完整性处理：违反约束时拒绝或级联
+ *
+ * */
+
 int integrity_manager_entity_constraint_check(integrity_manager *integrityManager, table_info *tableInfo, record_value *recordValue, sql_update_type sqlUpdateType){
 
 };
