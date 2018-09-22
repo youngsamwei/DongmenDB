0e492b98ef219cc6c7689037703362e3cd28b15a
youngsamwei <w3wei@sina.com>
2018-09-22 09:52:49 +0800

    /*  * 索引专题的课程设计，分为以下几个题目：  * 1. create index语句的解析：exp_06_01_create_index.c  * 2  索引的存储管理：exp_06_02_index.c  * 3  索引的更新：exp_06_03_index_update.c  * 4  基于索引的连接操作：exp_07_03_join_index.c  * 5  基于索引的物理查询优化：exp_08_01_index_select.c  *  * */

--- src_experiment/exp_06_index/exp_06_01_create_index.c
+++ src_experiment/exp_06_index/exp_06_01_create_index.c
@@ -2,6 +2,15 @@
 // Created by Sam on 2018/2/13.
 //
 
+/*
+ * 索引专题的课程设计，分为以下几个题目：
+ * 1. create index语句的解析：exp_06_01_create_index.c
+ * 2  索引的存储管理：exp_06_02_index.c
+ * 3  索引的更新：exp_06_03_index_update.c
+ * 4  基于索引的连接操作：exp_07_03_join_index.c
+ * 5  基于索引的物理查询优化：exp_08_01_index_select.c
+ *
+ * */
 
 /**
  * 实现create索引的解析
