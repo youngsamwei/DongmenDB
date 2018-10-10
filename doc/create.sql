d3f313f0635dac6557479cb2fc48da1af9f6b04a
Kwen <maokwen@live.com>
2018-10-10 16:37:34 +0800

    demodb student 添加性别字段

--- doc/create.sql
+++ doc/create.sql
@@ -1,20 +1,20 @@
 .open demodb
-create table student(sno char(10), sname char(20), sage integer);
+create table student(sno char(10), sname char(20), sage integer, ssex char(10));
 create table course(cno char(10), cname char(20), credit integer);
 create table sc(sno char(10), cno char(10), grade integer);
 
 .open demodb
 
 
-insert into student(sno, sname, sage) values("2012010101","tom simith",20);
-insert into student(sno, sname, sage) values("2012010102","jack simith",21);
-insert into student(sno, sname, sage) values("2012010103","li simith",22);
-insert into student(sno, sname, sage) values("2012010104","john simith",23);
-insert into student(sno, sname, sage) values("2012010105","dan simith",24);
-insert into student(sno, sname, sage) values("2012010106","mary simith",25);
-insert into student(sno, sname, sage) values("2012010107","jene simith",26);
-insert into student(sno, sname, sage) values("2012010108","di simith",27);
-insert into student(sno, sname, sage) values("2012010108","sam simith",27);
+insert into student(sno, sname, sage, ssex) values("2012010101","tom simith",20,"male");
+insert into student(sno, sname, sage, ssex) values("2012010102","jack simith",21,"male");
+insert into student(sno, sname, sage, ssex) values("2012010103","li simith",22,"male");
+insert into student(sno, sname, sage, ssex) values("2012010104","john simith",23,"male");
+insert into student(sno, sname, sage, ssex) values("2012010105","dan simith",24,"female");
+insert into student(sno, sname, sage, ssex) values("2012010106","mary simith",25,"female");
+insert into student(sno, sname, sage, ssex) values("2012010107","jene simith",26,"female");
+insert into student(sno, sname, sage, ssex) values("2012010108","di simith",27,"female");
+insert into student(sno, sname, sage, ssex) values("2012010108","sam simith",27,"male");
 
 insert into course(cno, cname, credit) values("c001", "data structure", 3);
 insert into course(cno, cname, credit) values("c002", "c", 4);
