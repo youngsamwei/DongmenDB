57170b1c6424030230cf474c67c3911d7675c48c
MaoKwen <maokwen@live.com>
2018-11-05 22:35:34 +0800

    Update filemanager.c

--- src/dongmendb/filemanager.c
+++ src/dongmendb/filemanager.c
@@ -100,7 +100,10 @@ int file_manager_closefile(file_manager *fileManager, char *fileName){
 }
 
 int _iter_closefile(char * filename, void_ptr fnIterValue, void_ptr arg){
-    fclose((FILE *)fnIterValue);
+    if (!fclose((FILE *)fnIterValue)) {
+        return HMAP_S_OK;
+    }
+    return HMAP_E_FAIL;
 }
 
 int file_manager_closeallfile(file_manager *fileManager){
