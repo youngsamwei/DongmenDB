641b4f36967eb8d873561b4fc208275fb7d370ed
youngsamwei <w3wei@sina.com>
2018-11-08 07:22:38 +0800

    修改测试用例中delete语句，去掉delete中的from。

--- src_experiment/exp_01_stmt_parser/exp_01_05_delete_test.cpp
+++ src_experiment/exp_01_stmt_parser/exp_01_05_delete_test.cpp
@@ -11,15 +11,15 @@ class Exp_01_05_DeleteTest : public TestStmtParser {
 protected:
 
     virtual void SetUp() {
-        _m_list[0] = "delete from student where sname = 'tom simith'";
+        _m_list[0] = "delete student where sname = 'tom simith'";
         _m_list[1] = "select * from student where sname = 'tom simith'";
-        _m_list[2] = "delete from student where sno = '2012010102'";
+        _m_list[2] = "delete student where sno = '2012010102'";
         _m_list[3] = "select *  from student where sno = '2012010102'";
-        _m_list[4] = "delete from student  where sname = 'john simith' and ssex='male'";
+        _m_list[4] = "delete student  where sname = 'john simith' and ssex='male'";
         _m_list[5] = "select *  from student  where sname = 'john simith' and ssex='male'";
-        _m_list[6] = "delete from sc where sno = '2012010103' ";
+        _m_list[6] = "delete sc where sno = '2012010103' ";
         _m_list[7] = "select *  from sc where sno = '2012010103' ";
-        _m_list[8] = "delete from sc  where grade >= 80 and cno = 'c003'";
+        _m_list[8] = "delete sc  where grade >= 80 and cno = 'c003'";
         _m_list[9] = "select *  from sc  where grade >= 80 and cno = 'c003'";
     }
     const char *_m_list[11];
