e5d7e8eeca85ee74299653885e8018dbeb91ab09
youngsamwei <w3wei@sina.com>
2018-11-08 08:37:35 +0800

    注释部分输出语句

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -243,10 +243,10 @@ int dongmendb_shell_handle_create_table(dongmendb_shell_handle_sql_t *ctx, const
 
     if (status == DONGMENDB_OK) {
         transaction_commit(ctx->db->tx);
-        fprintf(stdout, "create  success!");
+//        fprintf(stdout, "create  success!");
         return DONGMENDB_OK;
     } else {
-        fprintf(stderr, "create  failed!");
+//        fprintf(stderr, "create  failed!");
         return DONGMENDB_ERROR_IO;
     }
 };
@@ -280,10 +280,10 @@ int dongmendb_shell_handle_insert_table(dongmendb_shell_handle_sql_t *ctx, const
 
     if (status == DONGMENDB_OK) {
         transaction_commit(ctx->db->tx);
-        fprintf(stdout, "insert  success!");
+//        fprintf(stdout, "insert  success!");
         return DONGMENDB_OK;
     } else {
-        fprintf(stderr, "insert  failed!");
+//        fprintf(stderr, "insert  failed!");
         return DONGMENDB_ERROR_IO;
     }
 };
@@ -389,7 +389,7 @@ int dongmendb_shell_handle_update_data(dongmendb_shell_handle_sql_t *ctx, const 
 
     if (count >= 0) {
         transaction_commit(ctx->db->tx);
-        fprintf(stdout, "update  success! %d line updated.", count);
+//        fprintf(stdout, "update  success! %d line updated.", count);
         return DONGMENDB_OK;
     } else {
         fprintf(stderr, "update  failed!");
