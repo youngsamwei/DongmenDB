1709be7b87a53c3b16255bcdd56547f0fd68f6a9
youngsamwei <w3wei@sina.com>
2018-11-12 08:04:03 +0800

    增加update与delete语句执行物理计划的注释

--- src_experiment/exp_07_physical_operate/exp_07_05_update.c
+++ src_experiment/exp_07_physical_operate/exp_07_05_update.c
@@ -3,7 +3,10 @@
 //
 #include "physicalplan/physicalplan.h"
 
-/*执行 update 语句*/
+/*执行 update 语句的物理计划，返回修改的记录条数
+ * 返回大于等于0的值，表示修改的记录条数；
+ * 返回小于0的值，表示修改过程中出现错误。
+ * */
 /*TODO: plan_execute_update， update语句执行*/
 int plan_execute_update(dongmendb *db, sql_stmt_update *sqlStmtUpdate , transaction *tx){
     /*删除语句以select的物理操作为基础实现。
@@ -13,4 +16,5 @@ int plan_execute_update(dongmendb *db, sql_stmt_update *sqlStmtUpdate , transact
 
     fprintf(stderr, "TODO: update is not implemented yet. in plan_execute_update \n");
 
+    return -1;
 };
--- src_experiment/exp_07_physical_operate/exp_07_06_delete.c
+++ src_experiment/exp_07_physical_operate/exp_07_06_delete.c
@@ -3,7 +3,10 @@
 //
 #include "physicalplan/physicalplan.h"
 
-/*执行delete语句*/
+/*执行delete语句的物理计划，返回删除的记录条数
+ * 返回大于等于0的值，表示删除的记录条数；
+ * 返回小于0的值，表示删除过程中出现错误。
+ * */
 
 int plan_execute_delete(dongmendb *db, sql_stmt_delete *sqlStmtDelete,  transaction *tx){
 
@@ -11,4 +14,6 @@ int plan_execute_delete(dongmendb *db, sql_stmt_delete *sqlStmtDelete,  transact
      * 1. 使用 sql_stmt_delete 的条件参数，调用 physical_scan_select_create 创建select的物理计划并初始化;
      * 2. 执行 select 的物理计划，完成 delete 操作
      * */
+
+    return -1;
 };
