83d52132f0828d4ac5a460f3f1e446762b633d41
FancyKings <35259519+FancyKings@users.noreply.github.com>
2019-09-28 19:56:54 +0800

    Revert "Merge pull request #1 from youngsamwei/mastercpp"

--- src_experiment/exp_01_stmt_parser/exp_01_04_update.cpp
+++ src_experiment/exp_01_stmt_parser/exp_01_04_update.cpp
@@ -4,7 +4,6 @@
 
 #include <dongmensql/sqlstatement.h>
 #include <parser/StatementParser.h>
-#include <cstring>
 
 /**
  * 在现有实现基础上，实现update from子句
@@ -18,46 +17,7 @@
  */
 
 /*TODO: parse_sql_stmt_update， update语句解析*/
-sql_stmt_update *UpdateParser::parse_sql_stmt_update()
-{
-//  fprintf(stderr, "TODO: update is not implemented yet. in parse_sql_stmt_update \n");
-  char *tableName = nullptr;
-  vector<char*> fields;
-  vector<Expression*> fieldsExpr;
-  SRA_t *where = nullptr;
+sql_stmt_update *UpdateParser::parse_sql_stmt_update() {
+//    fprintf(stderr, "TODO: update is not implemented yet. in parse_sql_stmt_update \n");
 
-  //使用matchToken匹配update
-  if(!this->matchToken(TOKEN_RESERVED_WORD, "update"))
-    return nullptr;
-
-  //获取表名tableName
-  Token *token = parseNextToken();
-  //判断表名是否是TOKEN_WORD类型
-  if(token->type == TOKEN_WORD)
-  {
-    tableName = new_id_name();
-    memmove(tableName, token->text, sizeof(token->text));
-    SRA_t *sraTableName = static_cast<SRA_t *>(malloc(sizeof(SRA_t)));
-    sraTableName->t = SRA_TABLE;
-    SRA_Table_t *sraTable = static_cast<SRA_Table_t *>(malloc(sizeof(sraTable)));
-    sraTable->ref = static_cast<TableReference_t *>(malloc(sizeof(TableReference_t)));
-    memmove(sraTable->ref->table_name, tableName, sizeof(tableName));
-
-    // TODO:将值传进where
-  }
-  else
-  {
-    char *message = "invalid sql: missing table name.";
-    memmove(this->parserMessage, message, sizeof(message));
-    return nullptr;
-  }
-
-  //使用matchToken匹配set
-  token = parseEatAndNextToken();
-  if(!this->matchToken(TOKEN_RESERVED_WORD, "set"))
-  {
-    char *message = "invalid sql: should be 'set'";
-    memmove(this->parserMessage, message, sizeof(message));
-    return nullptr;
-  }
 };
