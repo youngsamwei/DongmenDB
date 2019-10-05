31550c56c223021706c2a2a1afa3f22a8959ad41
FancyKings <1533577900@qq.com>
2019-10-05 16:50:47 +0800

    为关系代数优化实验添加必要的Transaction参数

--- src/test_base/test_stmt_opimized.cpp
+++ src/test_base/test_stmt_opimized.cpp
@@ -52,88 +52,90 @@ int opt_condition_test(SRA_t *sra) {
 }
 
 /*查找指定的columnName是否在fieldsName中，若在则返回1，否则返回0*/
-int opt_search_in_fields(vector<char*> fieldsName, const char *columnName){
+int opt_search_in_fields(vector<char *> fieldsName, const char *columnName) {
 
-    for (int i = 0 ; i <fieldsName.size();i++){
+    for (int i = 0; i < fieldsName.size(); i++) {
         const char *cf = fieldsName.at(i);
-        if (! strcmp(cf, columnName)){
+        if (!strcmp(cf, columnName)) {
             /*若相等*/
             return 1;
         }
     }
-    return  0;
+    return 0;
 }
 
 /*判断tableName.columnName是否在sra中*/
-int opt_field_test(SRA_t *sra, const char *tableName, const char * columnName, TableManager *tableManager){
-    switch (sra->t){
-        case SRA_TABLE:{
-            if (tableName == NULL){
-                TableInfo *ti = tableManager->table_manager_get_tableinfo(sra->table.ref->table_name, NULL);
-                vector<char*> fieldsName = ti->fieldsName;
+int opt_field_test(SRA_t *sra, const char *tableName, const char *columnName, TableManager *tableManager,
+                   Transaction *transaction) {
+    switch (sra->t) {
+        case SRA_TABLE: {
+            if (tableName == NULL) {
+                TableInfo *ti = tableManager->table_manager_get_tableinfo(sra->table.ref->table_name, transaction);
+                vector<char *> fieldsName = ti->fieldsName;
                 /*在fieldsName中查找columnName*/
                 return opt_search_in_fields(fieldsName, columnName);
             }
-            if (!strcmp(tableName, sra->table.ref->table_name)){
+            if (!strcmp(tableName, sra->table.ref->table_name)) {
                 return 1;
-            }else{
+            } else {
                 return 0;
             }
         }
-        case SRA_JOIN:{
-            int ret = opt_field_test(sra->join.sra1, tableName, columnName,tableManager);
-            if (ret){
+        case SRA_JOIN: {
+            int ret = opt_field_test(sra->join.sra1, tableName, columnName, tableManager, transaction);
+            if (ret) {
                 return ret;
-            }else {
-                return opt_field_test(sra->join.sra2, tableName, columnName, tableManager);
+            } else {
+                return opt_field_test(sra->join.sra2, tableName, columnName, tableManager, transaction);
             }
         }
         case SRA_SELECT:
-            return opt_field_test(sra->select.sra, tableName, columnName, tableManager);
+            return opt_field_test(sra->select.sra, tableName, columnName, tableManager, transaction);
         default:
             return 0;
     }
 }
 
 /* 判断expr中的每个字段是否在sra中, 若存在一个字段不在sra中则返回0， 否则返回1*/
-int opt_fields_test(Expression *expr, SRA_t *sra, TableManager *tableManager) {
-    if (expr == NULL){
+int opt_fields_test(Expression *expr, SRA_t *sra, TableManager *tableManager, Transaction *transaction) {
+    if (expr == NULL) {
         return 0;
     }
     if (expr->term != NULL) {
-        switch(expr->term->t){
+        switch (expr->term->t) {
             case TERM_COLREF:
-                return opt_field_test(sra, expr->term->ref->tableName, expr->term->ref->columnName, tableManager);
+                return opt_field_test(sra, expr->term->ref->tableName, expr->term->ref->columnName, tableManager,
+                                      transaction);
             default:
                 return 0;
         }
     } else {
-        return opt_fields_test(expr->nextexpr, sra, tableManager);
+        return opt_fields_test(expr->nextexpr, sra, tableManager, transaction);
     }
 }
 
-int opt_optimed_test(Expression *expr, SRA_t *sra, TableManager *tableManager) {
+int opt_optimed_test(Expression *expr, SRA_t *sra, TableManager *tableManager, Transaction *transaction) {
 
     switch (sra->t) {
         case SRA_JOIN:
             /*检测expr中的字段属性是否在某个操作对象中，若是则返回1*/
-            return opt_fields_test(expr, sra, tableManager);
+            return opt_fields_test(expr, sra, tableManager, transaction);
         default:
             return 0;
     }
 }
 
 /*检测每个SRA_Select是否在最优位置*/
-int opt_test(SRA_t *sra, TableManager *tableManager) {
+int opt_test(SRA_t *sra, TableManager *tableManager, Transaction *transaction) {
     switch (sra->t) {
         case SRA_SELECT: {
             /*寻找子树中JOIM操作，若条件属性均包含在JOIN操作的某个分支中，则返回1*/
-            int ret = opt_optimed_test(sra->select.cond, sra->select.sra, tableManager);
+            int ret = opt_optimed_test(sra->select.cond, sra->select.sra, tableManager, transaction);
             if (ret) {
                 return ret;
             } else {
                 /*检查子树中的SELECT*/
-                return opt_test(sra->select.sra, tableManager);
+                return opt_test(sra->select.sra, tableManager, transaction);
             }
         };
         case SRA_TABLE: {
@@ -141,15 +143,15 @@ int opt_test(SRA_t *sra, TableManager *tableManager) {
         }
         case SRA_JOIN: {
 
-            int ret = opt_test(sra->join.sra1, tableManager);
+            int ret = opt_test(sra->join.sra1, tableManager, transaction);
             if (ret) {
                 return ret;
             }
-            return opt_test(sra->join.sra2, tableManager);
+            return opt_test(sra->join.sra2, tableManager, transaction);
 
         }
         case SRA_PROJECT:
-            return opt_test(sra->project.sra, tableManager);
+            return opt_test(sra->project.sra, tableManager, transaction);
         default:/*其他操作*/
             return 0;
     }
@@ -160,11 +162,12 @@ int TestStmtOptimized::opt_condition_pushdown_test(const char *sqlselect) {
     Parser *parser = new Parser(tokenizer);
     memset(parser->parserMessage, 0, sizeof(parser->parserMessage));
 
-    SelectParser * sp = new SelectParser(tokenizer);
+    SelectParser *sp = new SelectParser(tokenizer);
     SRA_t *selectStmt = sp->parse_sql_stmt_select();
 
     SRA_t *optimizedStmt = dongmengdb_algebra_optimize_condition_pushdown(selectStmt,
-                                                                          test_db_ctx->db->tableManager);
+                                                                          test_db_ctx->db->tableManager,
+                                                                          test_db_ctx->db->tx);
 
     /*检查思路：查找每个SRA_Select， 检查：
      * 1 是否多条件且and连接，若是则返回1 */
@@ -176,7 +179,7 @@ int TestStmtOptimized::opt_condition_pushdown_test(const char *sqlselect) {
     }
 
     /* 2 检查每个SRA_Select 是否在最优位置,若不是则返回1*/
-    ret = opt_test(optimizedStmt, test_db_ctx->db->tableManager);
+    ret = opt_test(optimizedStmt, test_db_ctx->db->tableManager, test_db_ctx->db->tx);
 
     return ret;
 }
--- src_experiment/exp_05_algebra_opt/exp_05_02_condition_push_down.cpp
+++ src_experiment/exp_05_algebra_opt/exp_05_02_condition_push_down.cpp
@@ -13,7 +13,8 @@
 /*输入一个关系代数表达式，输出优化后的关系代数表达式
  * 要求：在查询条件符合合取范式的前提下，根据等价变换规则将查询条件移动至合适的位置。
  * */
-SRA_t *dongmengdb_algebra_optimize_condition_pushdown(SRA_t *sra, TableManager *tableManager){
+SRA_t *
+dongmengdb_algebra_optimize_condition_pushdown(SRA_t *sra, TableManager *tableManager, Transaction *transaction) {
 
     /*初始关系代数语法树sra由三个操作构成：SRA_PROJECT -> SRA_SELECT -> SRA_JOIN，即对应语法树中三个节点。*/
 
