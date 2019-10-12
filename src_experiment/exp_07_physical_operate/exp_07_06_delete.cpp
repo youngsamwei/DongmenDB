3234f5fba10d4518f6c160896342849e35eb81cb
Shixuan Liu <420199938@qq.com>
2019-10-12 21:05:09 +0800

    完成delete物理计划(留下了一个不知道是不是bug的东西)

--- src_experiment/exp_07_physical_operate/exp_07_06_delete.cpp
+++ src_experiment/exp_07_physical_operate/exp_07_06_delete.cpp
@@ -12,10 +12,25 @@
  * 返回小于0的值，表示删除过程中出现错误。
  * */
 
-int ExecutionPlan::executeDelete(DongmenDB *db, sql_stmt_delete *sqlStmtDelete, Transaction *tx){
-        /*删除语句以select的物理操作为基础实现。
-     * 1. 使用 sql_stmt_delete 的条件参数，调用 physical_scan_select_create 创建select的物理计划并初始化;
-     * 2. 执行 select 的物理计划，完成 delete 操作
-     * */
-    return -1;
-};
+int ExecutionPlan::executeDelete(DongmenDB *db, sql_stmt_delete *sqlStmtDelete, Transaction *tx)
+{
+  /*删除语句以select的物理操作为基础实现。
+   * 1. 使用 sql_stmt_delete 的条件参数，调用 physical_scan_select_create 创建select的物理计划并初始化;
+   * 2. 执行 select 的物理计划，完成 delete 操作
+   *
+   */
+  //记录删除的条数
+  size_t deleted_count = 0;
+  //创建一个扫描
+  Scan *scan = generateScan(db, sqlStmtDelete->where, tx);
+  //将扫描指针放到最前
+  scan->beforeFirst();
+  //利用循环进行遍历
+  while(scan->next())
+  {
+    ///warning:不知道是不是删除所有记录。。。
+    scan->deleteRecord();
+    ++deleted_count;
+  }
+  return deleted_count;
+}
