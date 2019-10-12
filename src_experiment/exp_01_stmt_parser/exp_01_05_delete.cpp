0b685e868567a7b603c69ab7df529cc0ef04a04e
Shixuan Liu <420199938@qq.com>
2019-10-12 20:45:49 +0800

    完场delete语句解析

--- src_experiment/exp_01_stmt_parser/exp_01_05_delete.cpp
+++ src_experiment/exp_01_stmt_parser/exp_01_05_delete.cpp
@@ -17,5 +17,55 @@
  */
 
 sql_stmt_delete *DeleteParser::parse_sql_stmt_delete(){
+  char *tableName = nullptr;
+  SRA_t *where = nullptr;
+  Token *token = this->parseNextToken();
+  //匹配delete
+  if(!this->matchToken(TOKEN_RESERVED_WORD, (char*)"delete"))
+  {
+    strcpy(this->parserMessage, "语法错误.");
+    return nullptr;
+  }
 
+  //获取表名
+  token = this->parseNextToken();
+  if(token->type != TOKEN_WORD)
+  {
+    strcpy(this->parserMessage, "语法错误.");
+    return nullptr;
+  }
+  tableName = new_id_name();
+  strcpy(tableName, token->text);
+
+  //如果没有where
+  TableReference_t *tableReference = TableReference_make(tableName, nullptr);
+  where = SRATable(tableReference);
+  token = this->parseNextToken();
+  if(token == nullptr || token->type == TOKEN_SEMICOLON)
+  {
+    auto *sqlStmtDelete = static_cast<sql_stmt_delete *>(calloc(sizeof(sqlStmtDelete), 1));
+    sqlStmtDelete->where = where;
+    sqlStmtDelete->tableName = tableName;
+    return sqlStmtDelete;
+  }
+
+  //匹配where
+  token = this->parseNextToken();
+  if(!this->matchToken(TOKEN_RESERVED_WORD, "where"))
+  {
+    strcpy(this->parserMessage, "语法错误.");
+    return nullptr;
+  }
+
+  //解析where子句中的条件表达式
+  token = this->parseNextToken();
+  Expression *expression = this->parseExpressionRD();
+  if(parserStateType == PARSER_WRONG)
+    return nullptr;
+  where = SRASelect(where, expression);
+
+  auto *sqlStmtDelete = static_cast<sql_stmt_delete *>(calloc(sizeof(sql_stmt_delete), 1));
+  sqlStmtDelete->where = where;
+  sqlStmtDelete->tableName = tableName;
+  return sqlStmtDelete;
 };
