3dd2ab327f1879b7adaba2c145ed149a2225fd79
Shixuan Liu <420199938@qq.com>
2019-10-18 17:00:16 +0800

    完成了delete的解析和物理计划

--- src_experiment/exp_07_physical_operate/exp_07_06_delete.cpp
+++ src_experiment/exp_07_physical_operate/exp_07_06_delete.cpp
@@ -17,12 +17,6 @@ int ExecutionPlan::executeDelete(DongmenDB *db, sql_stmt_delete *sqlStmtDelete, 
   /*删除语句以select的物理操作为基础实现。
    * 1. 使用 sql_stmt_delete 的条件参数，调用 generateSelect 创建select的物理计划并初始化;
    * 2. 执行 select 的物理计划，完成 delete 操作
-   *
-   * class sql_stmt_delete  : public SQLStatement{
-   * public:
-   *   char *tableName;
-   *   SRA_t *where;
-   * };
    */
   //记录删除的条数
   size_t deleted_count = 0;
@@ -30,8 +24,6 @@ int ExecutionPlan::executeDelete(DongmenDB *db, sql_stmt_delete *sqlStmtDelete, 
   Scan *scan = generateSelect(db, sqlStmtDelete->where, tx);
   //将扫描指针放到最前
   scan->beforeFirst();
-  SRA_print(sqlStmtDelete->where);
-  putchar('\n');
   //利用循环进行遍历
   while(scan->next())
   {
