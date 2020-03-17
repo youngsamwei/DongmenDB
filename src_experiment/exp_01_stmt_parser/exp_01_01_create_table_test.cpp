8d8a99546d4b1f1c4b3ae06859cb29b298bdf630
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2020-03-17 19:18:45 +0800

    拆分测试

--- src_experiment/exp_01_stmt_parser/exp_01_01_create_table_test.cpp
+++ src_experiment/exp_01_stmt_parser/exp_01_01_create_table_test.cpp
@@ -2,77 +2,64 @@
 // Created by Sam on 2018/2/11.
 //
 
-#include <cstdio>
 #include <gtest/gtest.h>
-#include <map>
 #include <parser/StatementParser.h>
 #include <parser/Tokenizer.h>
 #include <test/test_stmt_parser.h>
 
-class Exp_01_01_CreateTableTest : public testing::Test {
-protected:
+class CreateTableParserTest : public testing::Test {
+public:
+    string sql{"create table typeTest(colChar char(10), colInt integer);"};
+    sql_stmt_create expect{};
+    sql_stmt_create result{};
+
     void SetUp() override {
-        sqlList[0] = "create table typeTest(colChar char(10), colInt integer);";
-        expectList[0] = setupTest0();
-    }
+        /* char *sql = "create table typeTest(colChar char(10), colInt integer, colBoolean boolean, colDouble double, colText text);"; */
 
-    const char *sqlList[10]{};
-    sql_stmt_create expectList[10]{};
+        const char *tableName = "typeTest";
+        vector<char *> fieldsName{"colChar", "colInt" /* , "colBoolean", "colDouble", "colText" */ };
+        auto *columns = new map<string, FieldInfo *>();
+        columns->insert(pair<string, FieldInfo *>("colChar", new FieldInfo(DATA_TYPE_CHAR, 10, "colChar")));
+        columns->insert(pair<string, FieldInfo *>("colInt", new FieldInfo(DATA_TYPE_INT, INT_SIZE, "colInt")));
+        /* NOT IMPLEMENT YET
+        columns->insert(pair<string, FieldInfo *>("colBoolean", new Fi  eldInfo(DATA_TYPE_BOOLEAN, BOOLEAN_SIZE, "colBoolean")));
+        columns->insert(pair<string, FieldInfo *>("colDouble", new FieldInfo(DATA_TYPE_DOUBLE, DOUBLE_SIZE, "colDouble")));
+        columns->insert(pair<string, FieldInfo *>("colText", new FieldInfo(DATA_TYPE_TEXT, TEXT_SIZE, "colText")));
+         */
+        expect.tableInfo = new TableInfo(tableName, fieldsName, columns);
+        expect.constraints = nullptr;
 
-    static sql_stmt_create parse(const char *sql) {
-        auto *tokenizer = new Tokenizer(sql);
+        auto *tokenizer = new Tokenizer(sql.c_str());
         auto *cp = new CreateParser(tokenizer);
-        auto *sqlStmtCreate = cp->parse_sql_stmt_create();
-        return *sqlStmtCreate;
+        auto *stmt = cp->parse_sql_stmt_create();
+        result = *stmt;
     }
 
-private:
-    sql_stmt_create setupTest0();
 };
 
-TEST_F(Exp_01_01_CreateTableTest, Correct) {
+TEST_F(CreateTableParserTest, Test_01_TableName) {
 
-    try {
-        sql_stmt_create resultList[10];
-
-        resultList[0] = parse(sqlList[0]);
-
-        // 表名
-        EXPECT_TRUE(equal_table_name(resultList[0], expectList[0]));
-        // 字段名
-        EXPECT_TRUE(equal_fields_name(resultList[0], expectList[0]));
-        // 字段
-        EXPECT_TRUE(equal_fields(resultList[0], expectList[0]));
-        // 表信息
-        EXPECT_TRUE(equal_table_info(resultList[0], expectList[0]));
-        // 判断
-        EXPECT_TRUE(equal(resultList[0], expectList[0]));
-
-    } catch (const exception &e) {
-        cout << e.what() << endl;
-    }
+    // 表名
+    EXPECT_TRUE(equal_table_name(expect, result));
 }
 
-// region 测试用例
-
-sql_stmt_create Exp_01_01_CreateTableTest::setupTest0() {
-    /* char *sql = "create table typeTest(colChar char(10), colInt integer, colBoolean boolean, colDouble double, colText text);"; */
-
-    const char *tableName = "typeTest";
-    vector<char *> fieldsName{"colChar", "colInt" /* , "colBoolean", "colDouble", "colText" */ };
-    auto *columns = new map<string, FieldInfo *>();
-    columns->insert(pair<string, FieldInfo *>("colChar", new FieldInfo(DATA_TYPE_CHAR, 10, "colChar")));
-    columns->insert(pair<string, FieldInfo *>("colInt", new FieldInfo(DATA_TYPE_INT, INT_SIZE, "colInt")));
-    /* NOT IMPLEMENT YET
-    columns->insert(pair<string, FieldInfo *>("colBoolean", new Fi  eldInfo(DATA_TYPE_BOOLEAN, BOOLEAN_SIZE, "colBoolean")));
-    columns->insert(pair<string, FieldInfo *>("colDouble", new FieldInfo(DATA_TYPE_DOUBLE, DOUBLE_SIZE, "colDouble")));
-    columns->insert(pair<string, FieldInfo *>("colText", new FieldInfo(DATA_TYPE_TEXT, TEXT_SIZE, "colText")));
-     */
-    sql_stmt_create sqlStmtCreate{};
-    sqlStmtCreate.tableInfo = new TableInfo(tableName, fieldsName, columns);
-    sqlStmtCreate.constraints = nullptr;
-
-    return sqlStmtCreate;
+TEST_F(CreateTableParserTest, Test_01_FieldName) {
+    // 字段名
+    EXPECT_TRUE(equal_fields_name(expect, result));
 }
 
-// endregion
+TEST_F(CreateTableParserTest, Test_01_Field) {
+    // 字段
+    EXPECT_TRUE(equal_fields(expect, result));
+}
+
+TEST_F(CreateTableParserTest, Test_01_TableInfo) {
+    // 表信息
+    EXPECT_TRUE(equal_table_info(expect, result));
+}
+
+TEST_F(CreateTableParserTest, Test_01_Full) {
+    // 判断
+    EXPECT_TRUE(equal(expect, result));
+
+}
--- src_experiment/exp_01_stmt_parser/exp_01_03_select_test.cpp
+++ src_experiment/exp_01_stmt_parser/exp_01_03_select_test.cpp
@@ -2,227 +2,153 @@
 // Created by Sam on 2018/2/11.
 //
 
-
-#include <cstdio>
 #include <gtest/gtest.h>
-#include <map>
 #include <parser/StatementParser.h>
 #include <parser/Tokenizer.h>
 #include <test/test_stmt_parser.h>
 
-class Exp_01_03_SelectTest : public testing::Test {
-protected:
-    void SetUp() override {
-        // 基本查询
-        sqlList[0] = "select * from student";
-        // 投影的解析
-        sqlList[1] = "select student.* from student";
-        sqlList[2] = "select sno from student";
-        sqlList[3] = "select sno, sname from student";
-        // 条件表达式的解析
-        sqlList[4] = "select sno from student where sage < 25";
-        // 多表查询
-        sqlList[5] = "select sname, grade from student, sc where student.sno = sc.sno";
-        sqlList[6] = "select student.sno, sname, grade from student, sc where student.sno = sc.sno";
-        // 聚合查询
-        sqlList[7] = "select count(*) from students";
-        sqlList[8] = "select count(*) from student group by sage";
-        // 排序查询
-        sqlList[9] = "select sname, sage from student order by sage";
-
-        expectList[0] = setupTest0();
-        expectList[1] = setupTest1();
-        expectList[2] = setupTest2();
-        expectList[3] = setupTest3();
-        expectList[4] = setupTest4();
-        expectList[5] = setupTest5();
-        expectList[6] = setupTest6();
-        expectList[7] = setupTest7();
-        expectList[8] = setupTest8();
-        expectList[9] = setupTest9();
-    }
-
-    static SRA_t *parse(const char *sql) {
-        auto *tokenizer = new Tokenizer(sql);
-        auto *sp = new SelectParser(tokenizer);
-        auto *sra = sp->parse_sql_stmt_select();
-        return sra;
-    }
-
-    char *sqlList[10]{};
-    SRA_t *expectList[10]{};
-
-private:
-    static SRA_t *setupTest0();
-
-    static SRA_t *setupTest1();
-
-    static SRA_t *setupTest2();
-
-    static SRA_t *setupTest3();
-
-    static SRA_t *setupTest4();
-
-    static SRA_t *setupTest5();
-
-    static SRA_t *setupTest6();
-
-    static SRA_t *setupTest7();
-
-    static SRA_t *setupTest8();
-
-    static SRA_t *setupTest9();
-
-};
-
-TEST_F(Exp_01_03_SelectTest, Correct) {
-
-    try {
-        SRA_t *resultList[10];
-
-        resultList[0] = parse(sqlList[0]);
-        resultList[1] = parse(sqlList[1]);
-        resultList[2] = parse(sqlList[2]);
-        resultList[3] = parse(sqlList[3]);
-        resultList[4] = parse(sqlList[4]);
-        resultList[5] = parse(sqlList[5]);
-        resultList[6] = parse(sqlList[6]);
-        resultList[7] = parse(sqlList[7]);
-        resultList[8] = parse(sqlList[8]);
-        resultList[9] = parse(sqlList[9]);
-
-        EXPECT_TRUE(equal_sra(resultList[0], expectList[0]));
-        EXPECT_TRUE(equal_sra(resultList[1], expectList[1]));
-        EXPECT_TRUE(equal_sra(resultList[2], expectList[2]));
-        EXPECT_TRUE(equal_sra(resultList[3], expectList[3]));
-        EXPECT_TRUE(equal_sra(resultList[4], expectList[4]));
-        EXPECT_TRUE(equal_sra(resultList[5], expectList[5]));
-        EXPECT_TRUE(equal_sra(resultList[6], expectList[6]));
-        EXPECT_TRUE(equal_sra(resultList[7], expectList[7]));
-        EXPECT_TRUE(equal_sra(resultList[8], expectList[8]));
-        EXPECT_TRUE(equal_sra(resultList[9], expectList[9]));
-
-    } catch (const exception &e) {
-        cout << e.what() << endl;
-    }
+SRA_t *parse(const string &sql) {
+    auto *tokenizer = new Tokenizer(sql.c_str());
+    auto *sp = new SelectParser(tokenizer);
+    auto *sra = sp->parse_sql_stmt_select();
+    return sra;
 }
 
+TEST(SelectParserTest, Test_01_Base) {
+    string sql = "select * from student";
 
-// region 测试用例
-
-SRA_t *Exp_01_03_SelectTest::setupTest0() {
-    // select * from student
     TableReference_t *ref = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("*")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRATable(ref),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest1() {
-    // select student.* from student
+TEST(SelectParserTest, Test_02_Project) {
+    string sql = "select sno from student";
+
     TableReference_t *ref = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("student.*")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRATable(ref),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest2() {
-    // select sno from student
+TEST(SelectParserTest, Test_03_Project) {
+    string sql = "select sno from student";
+
     TableReference_t *ref = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("sno")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRATable(ref),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest3() {
-    // select sno, sname from student
+TEST(SelectParserTest, Test_04_Project) {
+    string sql = "select sno, sname from student";
+
     TableReference_t *ref = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("sno")))->parseExpressionRD(),
             (new Parser(new Tokenizer("sname")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRATable(ref),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest4() {
-    // select sno from student where sage < 25
+TEST(SelectParserTest, Test_05_Expression) {
+    string sql = "select sno from student where sage < 25";
+
     TableReference_t *ref = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("sno")))->parseExpressionRD()
     };
-
     Expression *whereExpr{
             (new Parser(new Tokenizer("sage < 25")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
-            SRASelect(
-                    SRATable(ref),
-                    whereExpr
-            ),
+    SRA_t *expect = SRAProject(
+            SRASelect(SRATable(ref), whereExpr),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest5() {
-    // select sname, grade from student, sc where student.sno = sc.sno
+TEST(SelectParserTest, Test_06_MultiTable) {
+    string sql = "select sname, grade from student, sc where student.sno = sc.sno";
+
     TableReference_t *ref1 = TableReference_make("student", nullptr);
     TableReference_t *ref2 = TableReference_make("sc", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("sname")))->parseExpressionRD(),
             (new Parser(new Tokenizer("grade")))->parseExpressionRD()
     };
-
     Expression *whereExpr{
             (new Parser(new Tokenizer("student.sno = sc.sno")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRASelect(
-                    SRAJoin(
-                            SRATable(ref1),
-                            SRATable(ref2),
-                            nullptr
-                    ),
+                    SRAJoin(SRATable(ref1), SRATable(ref2), nullptr),
                     whereExpr
             ),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest6() {
-    // select student.sno, sname, grade from student sc where student.sno = sc.sno
+TEST(SelectParserTest, Test_07_MultiTable) {
+    string sql = "select student.sno, sname, grade from student, sc where student.sno = sc.sno";
+
     TableReference_t *ref1 = TableReference_make("student", nullptr);
     TableReference_t *ref2 = TableReference_make("sc", nullptr);
     vector<Expression *> fieldsExpr{
@@ -230,43 +156,48 @@ SRA_t *Exp_01_03_SelectTest::setupTest6() {
             (new Parser(new Tokenizer("sname")))->parseExpressionRD(),
             (new Parser(new Tokenizer("grade")))->parseExpressionRD()
     };
-
     Expression *whereExpr{
             (new Parser(new Tokenizer("student.sno = sc.sno")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRASelect(
-                    SRAJoin(
-                            SRATable(ref1),
-                            SRATable(ref2),
-                            nullptr
-                    ),
+                    SRAJoin(SRATable(ref1), SRATable(ref2), nullptr),
                     whereExpr
             ),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest7() {
-    // select count(*) from students
+TEST(SelectParserTest, Test_08_Group) {
+    string sql = "select count(*) from students";
+
     TableReference_t *ref1 = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("count(*)")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRATable(ref1),
             fieldsExpr
     );
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest8() {
-    // select count(*) from student group by sage
+TEST(SelectParserTest, Test_09_Group) {
+    string sql = "select count(*) from student group by sage";
+
     TableReference_t *ref1 = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("count(*)")))->parseExpressionRD()
@@ -275,17 +206,23 @@ SRA_t *Exp_01_03_SelectTest::setupTest8() {
             (new Parser(new Tokenizer("sage")))->parseExpressionRD()
     };
 
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRATable(ref1),
             fieldsExpr
     );
-    project->project.group_by.assign(groupExpr.begin(), groupExpr.end());
+    expect->project.group_by.assign(groupExpr.begin(), groupExpr.end());
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
 
-SRA_t *Exp_01_03_SelectTest::setupTest9() {
-    // select sname, sage from student order by sage
+TEST(SelectParserTest, Test_10_Sort) {
+    string sql = "select sname, sage from student order by sage";
+
     TableReference_t *ref1 = TableReference_make("student", nullptr);
     vector<Expression *> fieldsExpr{
             (new Parser(new Tokenizer("sname")))->parseExpressionRD(),
@@ -294,14 +231,16 @@ SRA_t *Exp_01_03_SelectTest::setupTest9() {
     vector<Expression *> orderExpr{
             (new Parser(new Tokenizer("sage")))->parseExpressionRD()
     };
-
-    SRA_t *project = SRAProject(
+    SRA_t *expect = SRAProject(
             SRATable(ref1),
             fieldsExpr
     );
-    project->project.order_by.assign(orderExpr.begin(), orderExpr.end());
+    expect->project.order_by.assign(orderExpr.begin(), orderExpr.end());
 
-    return project;
+    SRA_t *result = parse(sql);
+    EXPECT_TRUE(equal_sra(expect, result))
+                        << "Expected:" << endl
+                        << expect << endl
+                        << "Actual:" << endl
+                        << result;
 }
-
-// endregion
