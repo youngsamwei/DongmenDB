f0dee092dae4feeb1a88bc06db5cae8f0e0f3212
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2020-03-18 21:11:03 +0800

    split test exp_07_05_update_test

--- src_experiment/exp_07_physical_operate/exp_07_05_update_test.cpp
+++ src_experiment/exp_07_physical_operate/exp_07_05_update_test.cpp
@@ -7,56 +7,72 @@
 #include <test/test_stmt_parser.h>
 #include <test/test_physical_operate.h>
 
-class Exp_07_05_UpdateTest : public TestPhysicalOperate {
+class UpdateOperateTest : public TestPhysicalOperate {
 protected:
     void SetUp() override {
-        _m_list[0] = "update student set sname = 'Tom Cruise' where sno = '2012010101' ";
-        _m_list[1] = "update student set sname = 'zhang simith' where sname = 'li simith' ";
-        _m_list[2] = "update student set sname = 'li simith', ssex= 'male' where sname = 'zhang simith'; ";
-        _m_list[3] = "update student set sname = 'zhang simith', ssex= 'male', sage = sage + 1 where sname = 'li simith'";
-        _m_list[4] = "update student set sname = 'li simith' where sname = 'zhang simith'";
-        _m_list[5] = "update student set sage = sage + 1 where sname = 'li simith'";
-        _m_list[6] = "update student set sage = sage + 1 ";
-        _m_list[7] = "update student set sage = 20 where sno = '2012010101'";
-        _m_list[8] = "update student set sname = 'To mCruise' where sage > 30; ";
-        _m_list[9] = "update student set sage = sage + 1 where sage > 30 and ssex = 'male'; ";
-
-        _expect_list[0] = 1;
-        _expect_list[1] = 1;
-        _expect_list[2] = 1;
-        _expect_list[3] = 1;
-        _expect_list[4] = 1;
-        _expect_list[5] = 1;
-        _expect_list[6] = 9;
-        _expect_list[7] = 1;
-        _expect_list[8] = 0;
-        _expect_list[9] = 0;
+        if (count++ == 0) {
+            createDB(dbname);
+            createTable();
+            insertData();
+            ctx = test_db_ctx;
+        } else {
+            test_db_ctx = ctx;
+        }
     }
 
-    const char *_m_list[15]{};
-    int _expect_list[15]{};
+    void TearDown() override {
+        if (count-- == 0) {
+            dropDB();
+        }
+    }
+
     const char *dbname = "test_demodb";
+public:
+    static dongmendb_shell_handle_sql_t *ctx;
+    static int count;
 };
 
-TEST_F(Exp_07_05_UpdateTest, Correct) {
-    try {
-        createDB(dbname);
-        createTable();
-        insertData();
+int UpdateOperateTest::count = 0;
+dongmendb_shell_handle_sql_t *UpdateOperateTest::ctx = nullptr;
 
-        EXPECT_EQ(_expect_list[0], update(_m_list[0]));
-        EXPECT_EQ(_expect_list[1], update(_m_list[1]));
-        EXPECT_EQ(_expect_list[2], update(_m_list[2]));
-        EXPECT_EQ(_expect_list[3], update(_m_list[3]));
-        EXPECT_EQ(_expect_list[4], update(_m_list[4]));
-        EXPECT_EQ(_expect_list[5], update(_m_list[5]));
-        EXPECT_EQ(_expect_list[6], update(_m_list[6]));
-        EXPECT_EQ(_expect_list[7], update(_m_list[7]));
-        EXPECT_EQ(_expect_list[8], update(_m_list[8]));
-        EXPECT_EQ(_expect_list[9], update(_m_list[9]));
+TEST_F(UpdateOperateTest, Test1) {
+    EXPECT_EQ(1, update("update student set sname = 'Tom Cruise' where sno = '2012010101'"));
+}
 
-        dropDB();
-    } catch (const std::exception &e) {
-        cout << e.what() << endl;
-    }
+TEST_F(UpdateOperateTest, Test2) {
+
+    EXPECT_EQ(1, update("update student set sname = 'zhang simith' where sname = 'li simith'"));
+}
+
+TEST_F(UpdateOperateTest, Test3) {
+    EXPECT_EQ(1, update("update student set sname = 'li simith', ssex= 'male' where sname = 'zhang simith'"));
+}
+
+TEST_F(UpdateOperateTest, Test4) {
+    EXPECT_EQ(1,
+              update("update student set sname = 'zhang simith', ssex= 'male', sage = sage + 1 where sname = 'li simith'"));
+}
+
+TEST_F(UpdateOperateTest, Test5) {
+    EXPECT_EQ(1, update("update student set sname = 'li simith' where sname = 'zhang simith'"));
+}
+
+TEST_F(UpdateOperateTest, Test6) {
+    EXPECT_EQ(1, update("update student set sage = sage + 1 where sname = 'li simith'"));
+}
+
+TEST_F(UpdateOperateTest, Test7) {
+    EXPECT_EQ(9, update("update student set sage = sage + 1"));
+}
+
+TEST_F(UpdateOperateTest, Test8) {
+    EXPECT_EQ(1, update("update student set sage = 20 where sno = '2012010101'"));
+}
+
+TEST_F(UpdateOperateTest, Test9) {
+    EXPECT_EQ(0, update("update student set sname = 'To mCruise' where sage > 30"));
+}
+
+TEST_F(UpdateOperateTest, Test10) {
+    EXPECT_EQ(0, update("update student set sage = sage + 1 where sage > 30 and ssex = 'male'"));
 }
