a500445b4261e6b6cfe336b4311d8eca02c8a4a9
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2020-03-19 16:14:07 +0800

    make tests in exp_07 independent

--- src_experiment/exp_07_physical_operate/exp_07_05_update_test.cpp
+++ src_experiment/exp_07_physical_operate/exp_07_05_update_test.cpp
@@ -2,7 +2,6 @@
 // Created by maokw on 2020-03-04.
 //
 
-#include <cstdio>
 #include <gtest/gtest.h>
 #include <test/test_stmt_parser.h>
 #include <test/test_physical_operate.h>
@@ -10,30 +9,18 @@
 class UpdateOperateTest : public TestPhysicalOperate {
 protected:
     void SetUp() override {
-        if (count++ == 0) {
             createDB(dbname);
             createTable();
             insertData();
-            ctx = test_db_ctx;
-        } else {
-            test_db_ctx = ctx;
-        }
     }
 
     void TearDown() override {
-        if (count-- == 0) {
             dropDB();
-        }
     }
 
     const char *dbname = "test_demodb";
-public:
-    static dongmendb_shell_handle_sql_t *ctx;
-    static int count;
 };
 
-int UpdateOperateTest::count = 0;
-dongmendb_shell_handle_sql_t *UpdateOperateTest::ctx = nullptr;
 
 TEST_F(UpdateOperateTest, Test1) {
     EXPECT_EQ(1, update("update student set sname = 'Tom Cruise' where sno = '2012010101'"));
@@ -45,7 +32,7 @@ TEST_F(UpdateOperateTest, Test2) {
 }
 
 TEST_F(UpdateOperateTest, Test3) {
-    EXPECT_EQ(1, update("update student set sname = 'li simith', ssex= 'male' where sname = 'zhang simith'"));
+    EXPECT_EQ(1, update("update student set sname = 'zhang simith', ssex= 'male' where sname = 'li simith'"));
 }
 
 TEST_F(UpdateOperateTest, Test4) {
@@ -54,7 +41,7 @@ TEST_F(UpdateOperateTest, Test4) {
 }
 
 TEST_F(UpdateOperateTest, Test5) {
-    EXPECT_EQ(1, update("update student set sname = 'li simith' where sname = 'zhang simith'"));
+    EXPECT_EQ(0, update("update student set sname = 'li simith' where sname = 'zhang simith'"));
 }
 
 TEST_F(UpdateOperateTest, Test6) {
@@ -74,5 +61,5 @@ TEST_F(UpdateOperateTest, Test9) {
 }
 
 TEST_F(UpdateOperateTest, Test10) {
-    EXPECT_EQ(0, update("update student set sage = sage + 1 where sage > 30 and ssex = 'male'"));
+    EXPECT_EQ(9, update("update student set sage = sage + 1 where sage > 10 and ssex = 'male'"));
 }
--- src_experiment/exp_07_physical_operate/exp_07_06_delete_test.cpp
+++ src_experiment/exp_07_physical_operate/exp_07_06_delete_test.cpp
@@ -2,7 +2,6 @@
 // Created by maokw on 2020-03-04.
 //
 
-#include <cstdio>
 #include <gtest/gtest.h>
 #include <test/test_stmt_parser.h>
 #include <test/test_physical_operate.h>
@@ -10,30 +9,18 @@
 class DeleteOperateTest : public TestPhysicalOperate {
 protected:
     void SetUp() override {
-        if (count++ == 0) {
             createDB(dbname);
             createTable();
             insertData();
-            ctx = test_db_ctx;
-        } else {
-            test_db_ctx = ctx;
-        }
     }
 
     void TearDown() override {
-        if (count-- == 0) {
             dropDB();
-        }
     }
 
     const char *dbname = "test_demodb";
-public:
-    static dongmendb_shell_handle_sql_t *ctx;
-    static int count;
 };
 
-int DeleteOperateTest::count = 0;
-dongmendb_shell_handle_sql_t *DeleteOperateTest::ctx = nullptr;
 
 TEST_F(DeleteOperateTest, Test1) {
     EXPECT_EQ(1, delete_("delete student where sname = 'tom simith'"));
@@ -48,7 +35,7 @@ TEST_F(DeleteOperateTest, Test3) {
 }
 
 TEST_F(DeleteOperateTest, Test4) {
-    EXPECT_EQ(0, delete_("select *  from student where sno = '2012010102'"));
+    EXPECT_EQ(0, delete_("select * from student where sno = '2012010102'"));
 }
 
 TEST_F(DeleteOperateTest, Test5) {
@@ -56,7 +43,7 @@ TEST_F(DeleteOperateTest, Test5) {
 }
 
 TEST_F(DeleteOperateTest, Test6) {
-    EXPECT_EQ(0, delete_("select *  from student  where sname = 'john simith' and ssex='male'"));
+    EXPECT_EQ(0, delete_("select * from student  where sname = 'john simith' and ssex='male'"));
 }
 
 TEST_F(DeleteOperateTest, Test7) {
@@ -64,13 +51,13 @@ TEST_F(DeleteOperateTest, Test7) {
 }
 
 TEST_F(DeleteOperateTest, Test8) {
-    EXPECT_EQ(0, delete_("select *  from sc where sno = '2012010103' "));
+    EXPECT_EQ(0, delete_("select * from sc where sno = '2012010103' "));
 }
 
 TEST_F(DeleteOperateTest, Test9) {
-    EXPECT_EQ(3, delete_("delete sc  where grade >= 80 and cno = 'c003'"));
+    EXPECT_EQ(3, delete_("delete sc where grade >= 80 and cno = 'c003'"));
 }
 
 TEST_F(DeleteOperateTest, Test10) {
-    EXPECT_EQ(0, delete_("select *  from sc  where grade >= 80 and cno = 'c003'"));
+    EXPECT_EQ(0, delete_("select * from sc where grade >= 80 and cno = 'c003'"));
 }
